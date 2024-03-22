1b186f3ad09e06fddf2340d3945f3c673cbd7007
poligenius <marco.97marini@gmail.com>
2024-03-22 01:43:14 +0100

    adjusted video loading

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Hero.css'
-import rainforestburning from './images/rainforestburning.mp4'
+
 
 const Hero = () => {
     return (
         <div className='hero'>
             <video className='background-video' autoPlay loop muted>
-                <source src={rainforestburning} type='video/mp4' />
+                <source src={process.env.PUBLIC_URL + '/images/rainforestburning.mp4'} type='video/mp4' />
             </video>
             <div className='content'>
                 <p>Save Amazon Rainforest</p>
