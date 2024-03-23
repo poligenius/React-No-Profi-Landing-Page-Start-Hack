eb281b1ad5b0fc46382aa42b4d8909dabb342676
poligenius <marco.97marini@gmail.com>
2024-03-23 18:46:19 +0100

    test to fix video on deployed site

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className='hero'>
             <video className='background-video' autoPlay loop muted>
-                <source src="/images/rainforestburning.mp4" type='video/mp4' />
+                <source src="https://main--put-off-fire.netlify.app/images/rainforestburning.mp4" type='video/mp4' />
             </video>
             <div className='content'>
                 <p>Save Amazon Rainforest</p>
